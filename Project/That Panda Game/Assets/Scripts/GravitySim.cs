880d4aea7bfb811545ce1336185067fc348f4a4a
TobenaiGemu <akuapengin42@gmail.com>
2018-09-28 15:31:12 +1000

    Planet\Player\Gravity
    Made a planet with gravity and a character that can walk around it

---
+++ Project/That Panda Game/Assets/Scripts/GravitySim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySim : MonoBehaviour
+{
+
+    private GameObject[] _bodies;
+
+    private Vector3 _velocity;
+    private Vector3 _direction;
+
+    private Rigidbody _rb;
+
+    public static float gravConstant = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _bodies = GameObject.FindGameObjectsWithTag("GravityForce");
+        _rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    foreach (GameObject obj in _bodies)
+        {
+            Rigidbody rb = obj.GetComponent<Rigidbody>();
+            if (obj != this.transform.gameObject)
+            {
+                _direction = rb.position - _rb.position;
+                float distance = _direction.magnitude;
+                float force = _rb.mass * rb.mass / Mathf.Pow(distance, 2) * gravConstant * Time.deltaTime;
+                _rb.AddForce(_direction.normalized * force, ForceMode.Impulse);
+            }
+        }
+	}
+}
