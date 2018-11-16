3f0395b8bbaa5d87555bf543bd14606aa6b6ab78
TobenaiGemu <akuapengin42@gmail.com>
2018-11-16 13:51:10 +1100

    Clouds move

---
+++ Project/That Panda Game/Assets/Scripts/CloudForeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudForeScript : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    private Vector2 _offset;
+
+    private Material _mat;
+
+    private void Awake()
+    {
+        _mat = GetComponent<Renderer>().material;
+        _offset = Vector3.zero;
+    }
+
+    private void Update ()
+    {
+        _offset.x += _speed * Time.deltaTime;
+        _mat.SetTextureOffset("_MainTex", _offset);
+	}
+}
