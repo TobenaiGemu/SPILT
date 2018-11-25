4393488135f542a683c9a89dc47cc9343aa3a392
Tobenai <akuapengin42@gmail.com>
2018-11-25 15:50:57 +1100

    Cookie/Apple sound effects added

--- Project/That Panda Game/Assets/Scripts/Pickups/AppleAction.cs
+++ Project/That Panda Game/Assets/Scripts/Pickups/AppleAction.cs
@@ -7,8 +7,16 @@ public class AppleAction : MonoBehaviour
     [SerializeField]
     private int _coinDrop;
 
+    private AudioSource _appleSound;
+
+    private void Awake()
+    {
+        _appleSound = GameObject.Find("InGameSounds").transform.Find("Apple").GetComponent<AudioSource>();
+    }
+
     public void DropCoinFromCharacter(Character character)
     {
+        _appleSound.Play();
         character.DropCoins(_coinDrop);
         character.StartSickBubbles();
     }
--- Project/That Panda Game/Assets/Scripts/Pickups/SpeedAction.cs
+++ Project/That Panda Game/Assets/Scripts/Pickups/SpeedAction.cs
@@ -10,8 +10,16 @@ public class SpeedAction : MonoBehaviour
     [SerializeField]
     private float _speedDuration;
 
+    private AudioSource _cookieSound;
+
+    private void Awake()
+    {
+        _cookieSound = GameObject.Find("InGameSounds").transform.Find("Cookie").GetComponent<AudioSource>();
+    }
+
     public void MultiplyCharacterSpeed(Character character)
     {
+        _cookieSound.Play();
         character.MultiplySpeed(_speedMultiplier, _speedDuration);
         character.StartWhooshing();
     }
